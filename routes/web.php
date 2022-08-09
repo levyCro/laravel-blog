b7fed751bfaf88b7e49024cf5465c80ce3055138
Levy Crols <103933738+levyCro@users.noreply.github.com>
2022-08-09 10:45:58 +0200

    adding a collect build in function to map my posts

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,34 @@ use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
-   $files = File::files(resource_path("posts"));
-   $posts= [];
 
-   foreach ($files as $file) {
-    $document = YamlFrontMatter::parseFile($file);
+    $posts = collect(File::files(resource_path("posts")))
+    ->map(function ($file) {
+        $document = YamlFrontMatter::parseFile($file);
 
-    $posts[] = new Post(
-        $document->title,
-        $document->excerpt,
-        $document->date,
-        $document->body,
-        $document->slug,
-    );
-   }
-   // $posts = Post::all();
+        return new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug,
+        );
+    });
+
+
+
+    //    $posts = array_map(function ($file){
+    //      $document = YamlFrontMatter::parseFile($file);
+
+    //     return new Post(
+    //         $document->title,
+    //         $document->excerpt,
+    //         $document->date,
+    //         $document->body,
+    //         $document->slug,
+    //     );
+    //    }, $files);
+
 
     return view('posts', [
         'posts' => $posts
@@ -32,5 +45,4 @@ Route::get('posts/{post}', function ($slug) {
     return view('post', [
         'post' => Post::find($slug)
     ]);
- 
 })->where('post', '[A-z_\-]+');
