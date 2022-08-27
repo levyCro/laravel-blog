71a625827b1e74660febec3c8788804fc1c6a827
Levy Crols <103933738+levyCro@users.noreply.github.com>
2022-08-27 14:55:40 +0200

    release

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -8,5 +8,5 @@
            class="border border-green-300 p-2 w-full rounded"
            {{ $attributes(['value' => old($name)]) }}>
   
-    <x-form.error name="{{ $name }}"/>
+{{-- <x-form.error name="{{ $name }}"/>     --}}
 </x-form.field>
