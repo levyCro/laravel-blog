456b41259723de6dc732c0421d44a537e02e73c9
Levy Crols <103933738+levyCro@users.noreply.github.com>
2022-08-28 11:49:05 +0200

    Add Heroku creds and update default route to hit the DB.

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,14 @@ use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 
-
+Route::get('/', function()
+{
+    return User::all();
+});
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
